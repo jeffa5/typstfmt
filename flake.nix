e5c0412b39fe38e27520dd2537ec23df957f10d6
Andrew Jeffery <dev@jeffas.io>
2023-07-08 14:16:57 +0100

    Add nix package default

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
+    crate2nix.url = "github:jeffa5/crate2nix";
   };
 
   outputs = {
@@ -12,6 +13,7 @@
     nixpkgs,
     rust-overlay,
     flake-utils,
+    crate2nix,
   }:
     flake-utils.lib.eachDefaultSystem
     (
@@ -25,17 +27,24 @@
         cargoNix = import ./Cargo.nix {inherit pkgs;};
         workspacePackages = lib.attrsets.mapAttrs (name: value: value.build) cargoNix.workspaceMembers;
       in {
-        packages = workspacePackages;
+        packages =
+          workspacePackages
+          // {
+            default = workspacePackages.typstfmt;
+          };
 
         formatter = pkgs.alejandra;
 
         devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            (rust.override {extensions = ["rust-src"];})
-            cargo-watch
-            cargo-fuzz
-            crate2nix
-          ];
+          buildInputs = with pkgs;
+            [
+              (rust.override {extensions = ["rust-src"];})
+              cargo-watch
+              cargo-fuzz
+            ]
+            ++ [
+              crate2nix.packages.${system}.crate2nix
+            ];
         };
       }
     );
