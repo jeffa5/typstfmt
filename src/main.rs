304a4ee49fac425fdce0941ff071221cc0aeed54
Andrew Jeffery <dev@jeffas.io>
2023-07-09 22:13:20 +0100

    Add check mode

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::fs::File;
 use std::io::Read;
 use std::io::Write;
 use std::path::PathBuf;
+use tracing::info;
+use tracing::warn;
 use typstfmt::format;
 
 use clap::Parser;
@@ -14,16 +16,27 @@ struct Args {
 
     #[arg(long, default_value = "typstfmt.toml")]
     config_path: PathBuf,
+
+    /// Run in 'check' mode. Exits with 0 if all input is formatted correctly. Exits with 1 if formatting of any input is required.
+    #[arg(long)]
+    check: bool,
 }
 
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
+
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .init();
+
     let paths = if !args.files.is_empty() {
         args.files
     } else {
         let glob = globmatch::Builder::new("**.typ").build(".").unwrap();
         glob.into_iter().flat_map(|path| path.ok()).collect()
     };
+
+    let mut check_ok = true;
     for path in paths.into_iter() {
         let mut file = File::options()
             .read(true)
@@ -42,12 +55,24 @@ fn main() -> anyhow::Result<()> {
             typstfmt::Config::default()
         };
 
-        let formatted = format(&content, config)?;
-        let mut file = File::create(&path)?;
-        file.write_all(formatted.as_bytes())?;
+        info!(?path, "Formatting input");
 
+        let formatted = format(&content, config)?;
+        if args.check {
+            if formatted != content {
+                warn!(?path, "Input needs formatting");
+                check_ok = false;
+            } else {
+                info!(?path, "Input is already formatted");
+            }
+        }
         let mut file = File::create(&path)?;
         file.write_all(formatted.as_bytes())?;
     }
+
+    if args.check && !check_ok {
+        anyhow::bail!("Failed check");
+    }
+
     Ok(())
 }
