2a8a0a0744b00bd2e1f9db75109e88ee4d7691ae
Andrew Jeffery <dev@jeffas.io>
2023-07-09 22:05:19 +0100

    cli: Updates

--- src/main.rs
+++ src/main.rs
@@ -7,27 +7,19 @@ use typstfmt::format;
 use clap::Parser;
 
 #[derive(Parser, Debug)]
-#[command(version = "0.0.1", about = "A formatter for the typst language")]
+#[command(version = "0.0.1", about = "Format typst code")]
 struct Args {
     /// A file to format. If not specified, all .typ file will be formatted
-    #[arg()]
-    input: Option<PathBuf>,
-
-    /// If specified, the result of output will be put in this file. input *must* be specified if you set output.
-    #[arg(short, long)]
-    output: Option<PathBuf>,
+    files: Vec<PathBuf>,
 
     #[arg(long, default_value = "typstfmt.toml")]
-    config: PathBuf,
+    config_path: PathBuf,
 }
 
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
-    if args.output.is_some() && args.input.is_none() {
-        panic!("Input must be specified to use an output.")
-    }
-    let paths = if let Some(input) = args.input {
-        vec![input]
+    let paths = if !args.files.is_empty() {
+        args.files
     } else {
         let glob = globmatch::Builder::new("**.typ").build(".").unwrap();
         glob.into_iter().flat_map(|path| path.ok()).collect()
@@ -36,13 +28,13 @@ fn main() -> anyhow::Result<()> {
         let mut file = File::options()
             .read(true)
             .open(&path)
-            .unwrap_or_else(|e| panic!("Couldn't open input file : {e}"));
+            .unwrap_or_else(|e| panic!("Couldn't open file : {e}"));
         let mut content = String::with_capacity(1024);
         file.read_to_string(&mut content)?;
         drop(file);
 
-        let config = if args.config.is_file() {
-            let mut config_file = File::open(&args.config)?;
+        let config = if args.config_path.is_file() {
+            let mut config_file = File::open(&args.config_path)?;
             let mut config_file_content = String::new();
             config_file.read_to_string(&mut config_file_content)?;
             toml::from_str(&config_file_content)?
@@ -51,11 +43,8 @@ fn main() -> anyhow::Result<()> {
         };
 
         let formatted = format(&content, config)?;
-        if let Some(output) = args.output {
-            let mut file = File::create(output)?;
-            file.write_all(formatted.as_bytes())?;
-            break;
-        }
+        let mut file = File::create(&path)?;
+        file.write_all(formatted.as_bytes())?;
 
         let mut file = File::create(&path)?;
         file.write_all(formatted.as_bytes())?;
