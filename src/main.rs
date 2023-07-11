13edeab300b70348271864f34f5dc087a928c0fd
Andrew Jeffery <dev@jeffas.io>
2023-07-11 08:32:11 +0100

    Update cli reporting

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,21 @@ use tracing::metadata::LevelFilter;
 use tracing::warn;
 use tracing_subscriber::EnvFilter;
 use typstfmt::format;
+use typstfmt::FormatError;
 
 use clap::Parser;
+
+#[derive(Debug, thiserror::Error)]
+enum Error {
+    #[error("format error: {0}")]
+    FormatError(#[from] FormatError),
+    #[error("check failed")]
+    CheckFailed,
+    #[error("io error: {0}")]
+    IOError(#[from] std::io::Error),
+    #[error("toml config error: {0}")]
+    TomlError(#[from] toml::de::Error),
+}
 
 #[derive(Parser, Debug)]
 #[command(version = "0.1.0", about = "Format typst code")]
@@ -58,27 +71,58 @@ fn main() -> anyhow::Result<()> {
         glob.into_iter().flat_map(|path| path.ok()).collect()
     };
 
-    let mut check_ok = true;
+    let mut ok = 0;
+    let mut erroneous = 0;
+    let mut needs_formatting = 0;
     for path in paths.into_iter() {
         match format_file(&path, &args) {
             Ok(()) => {
                 info!(?path, "Successfully formatted file");
+                ok += 1;
             }
             Err(error) => {
-                warn!(?path, ?error, "Failed to format file");
-                check_ok = false;
+                match error {
+                    Error::FormatError(_) => {
+                        warn!(?path, %error, "Failed to format file");
+                        erroneous += 1;
+                    }
+                    Error::CheckFailed => {
+                        warn!(?path, "Failed check");
+                        needs_formatting += 1;
+                    }
+                    Error::IOError(_) => {
+                        warn!(?path, %error, "Got an error")
+                    }
+                    Error::TomlError(_) => {
+                        warn!(?path, %error, "Failed to get config")
+                    }
+                }
             }
         }
     }
 
-    if args.check && !check_ok {
-        anyhow::bail!("Failed check");
+    if args.check {
+        if needs_formatting > 0 {
+            anyhow::bail!(
+                "Failed check. {} files passed, {} had errors, {} need formatting",
+                ok,
+                erroneous,
+                needs_formatting
+            );
+        } else {
+            println!(
+                "Passed check. {} files passed, {} had errors",
+                ok, erroneous
+            );
+        }
+    } else {
+        println!("Formatted {} files. {} files had errors", ok, erroneous);
     }
 
     Ok(())
 }
 
-fn format_file(path: &Path, args: &Args) -> anyhow::Result<()> {
+fn format_file(path: &Path, args: &Args) -> Result<(), Error> {
     let mut file = File::options()
         .read(true)
         .open(path)
@@ -101,7 +145,7 @@ fn format_file(path: &Path, args: &Args) -> anyhow::Result<()> {
     let formatted = format(&content, config)?;
     if args.check {
         if formatted != content {
-            anyhow::bail!("Output still needs formatting");
+            return Err(Error::CheckFailed);
         }
     } else {
         let mut file = File::create(path)?;
